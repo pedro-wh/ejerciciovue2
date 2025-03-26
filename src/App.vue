9df251899254a4c5e7a98906ff497f1797b7ed24
Pedro Jesús León Solís <pedro@wisphub.net>
2025-03-26 12:47:08 -0500

    Agregando el vue + vite

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>This is a {{ message }}</h1>
+    <p>...and vite btw</p>
+  </div>
+</template>
+
+
+
+<script>
+export default{
+  data(){
+    return {
+      message: "vue view"
+    }
+  }
+}
+</script>
