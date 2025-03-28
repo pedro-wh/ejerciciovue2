fc933468286f2e607db0bbafac335415362d5a73
pedro-wh <pedro@wisphub.net>
2025-03-28 07:24:41 -0500

    Avances

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@
                     {{ item.name }}
                   </span>
                   <input v-else v-focus type="text" v-model="item.name" @blur="disableEdit(item)" @keyup.enter="disableEdit(item)" style="z-index: 9;" />
-                  <button class="btn btn-light p-1 py-0 supertacha"><i class="fa-solid fa-xmark"></i></button>
+                  <button @click="deleteTodo(item.id)" class="btn btn-light p-1 py-0 supertacha"><i class="fa-solid fa-xmark"></i></button>
                 </div>
               </VueDraggable>
               <button @click="addTodo(column.id)" type="button" class="btn btn-light"><i class="fa-solid fa-plus me-1"></i> Agregar nueva tarea</button>
@@ -71,25 +71,32 @@ export default {
     }
   },
   methods: {
-    async addTodo(columnId){
-      const newTodo = {id: this.temporaryFinalId++, name: "Tarea Nueva", idColumn:columnId}
+    async addTodo(columnId) {
+      const newTodo = { id: this.temporaryFinalId++, name: "Tarea Nueva", idColumn: columnId }
+      //Aqui hacer un post
       const column = this.columns.find(x => x.id === columnId)
-      if(column){
+      if (column) {
         column.todos.push(newTodo)
       }
     },
-    enableEdit(item){
+    enableEdit(item) {
       this.$set(item, 'editing', true)
     },
     async disableEdit(item) {
       item.editing = false;
+      //Aqui hacer un update
     },
-    async moveTodo(event, columnId){
-      // debugger
-      // const todo = this.columns.flatMap(x => x.todos).find(y => y.id === event.data.id)
-      // if(todo){
-      //   todo.idColumn = columnId
-      // }
+    async moveTodo(event, columnId) {
+      this.$nextTick(() => {
+        const todo = this.columns.flatMap(x => x.todos).find(y => y.id === event.data.id)
+        if (todo) {
+          todo.idColumn = columnId
+        }
+        //Aqui hacer un update move
+      });
+    },
+    async deleteTodo() {
+      
     }
   }
 }
@@ -100,16 +107,19 @@ export default {
 .coolhoverpedrito {
   position: relative;
 }
-.coolhoverpedrito:hover .supertacha  {
+
+.coolhoverpedrito:hover .supertacha {
   visibility: visible;
   z-index: 4;
   opacity: 0.2;
 }
+
 .supertacha {
- position: absolute;
- right: 10px;
- visibility: hidden;
+  position: absolute;
+  right: 10px;
+  visibility: hidden;
 }
+
 .ghost {
   opacity: 0.2;
 }
