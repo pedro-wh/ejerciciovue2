cfcadfc582bdfe6781b88dce7267073c18ede444
Pedro Jesús León Solís <pedro@wisphub.net>
2025-03-29 13:58:57 -0500

    Avances con el codigo

--- htmx/urls.py
+++ htmx/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from htmx.views import index
+from htmx.views import htmx_delete, index
 
 
 urlpatterns = [
     url(r'^$', index, name="index"),
+    url(r'^htmx_delete/(?P<id>\d+)/$', htmx_delete, name="index"),
 ]
--- htmx/views.py
+++ htmx/views.py
@@ -1,9 +1,20 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
-from api.models import Column
+from api.models import Column, Todo
 
 # Create your views here.
 def index(request):
   columns = Column.objects.prefetch_related("todos").all()
   context = {"columns": columns}
   return render(request, "htmx/index.html", context)
+
+
+def htmx_delete(request, id):
+  if request.method == "DELETE":
+    todo = get_object_or_404(Todo, id=id)
+    todo.delete()
+    return HttpResponse(status=204)
+  
+  return HttpResponse(status=405)
