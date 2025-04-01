d5af79b94ba82fe3ee31773c58d95c9575ecec99
Pedro Jesús León Solís <pedro@wisphub.net>
2025-04-01 12:43:51 -0500

    Avances con jquery

--- jquery/urls.py
+++ jquery/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 
-from jquery.views import index
+from jquery.views import jquery_createTodo, jquery_delete, index, jquery_moveTodo
 
 
 urlpatterns = [
     url(r'^$', index, name="index"),
+    url(r'^jquery_delete/(?P<id>\d+)/$', jquery_delete, name="index"),
+    url(r'^jquery_createTodo/(?P<id>\d+)/$', jquery_createTodo, name="create"),
+    url(r'^jquery_moveTodo/(?P<idTodo>\d+)/(?P<idColumn>\d+)/$', jquery_moveTodo, name="edit"),
 ]
--- jquery/views.py
+++ jquery/views.py
@@ -1,9 +1,31 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import get_object_or_404, render
 
-from api.models import Column
+from api.models import Column, Todo
 
 # Create your views here.
 def index(request):
   columns = Column.objects.prefetch_related("todos").all()
   context = {"columns": columns}
   return render(request, "jquery/index.html", context)
+
+
+def jquery_delete(request, id):
+  if request.method == "DELETE":
+    todo = get_object_or_404(Todo, id=id)
+    todo.delete()
+    return HttpResponse(status=200)
+  return HttpResponse(status=200)
+
+def jquery_createTodo(request, id):
+  if request.method == "POST":
+    todo = Todo(column_id=id, name="Tarea Nueva")
+    todo.save()
+    return JsonResponse({"id": todo.id, "name": todo.name, "column_id": todo.column_id})
+
+
+def jquery_moveTodo(request, idTodo, idColumn):
+  todo = Todo.objects.get(id=idTodo)
+  todo.column_id = idColumn
+  todo.save()
+  return HttpResponse(status=200)
